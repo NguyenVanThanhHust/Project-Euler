19004d980255e8a74962cd06fe429c49035e4c36
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-07-15 09:01:08 +0700

    1st commit

---
+++ Programming Challenges Steven Skiena/Chapter 13 Geometry/Faster_than_speeding_bullet.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+struct point
+{
+    float x, y;
+}
+
+struct line
+{
+    float a, b, c;
+}
+
+struct circle
+{
+    point center;
+    float radius;
+}
+
+float distance_point_to_point(point &1p, point &p2);
+float distance_point_to_line(point &p, line &l)
+int main()
+{
+    ios_basse::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    return 0;
+}
